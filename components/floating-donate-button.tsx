59d617d93a9c7e46407e0bea7cc605bff956278f
Habib74 <habeeb@microlent.com>
2025-08-15 01:27:02 +0530

    donate button

--- components/floating-donate-button.tsx
+++ components/floating-donate-button.tsx
@@ -14,7 +14,7 @@ export function FloatingDonateButton() {
       className="floating-donate bg-gradient-to-r from-yellow-500 to-yellow-600 hover:from-yellow-600 hover:to-yellow-700 text-white shadow-2xl rounded-full px-6 py-3 text-sm font-semibold"
       size="lg"
     >
-      <HandHeart className="w-5 h-5 mr-2" />
+      <HandHeart className="w-5 h-5 mr-0" />
       Donate Now
     </Button>
   )
